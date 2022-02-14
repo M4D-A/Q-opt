5595b9c2be89f3ea45acba3cf8ee79f963823c29
M4D-A <jagielski.adamm@gmail.com>
2022-02-14 19:33:38 +0100

    Added Circuit Class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+//
+// Created by M4DA, jagielski.adamm@gmail.com
+//
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "Circuit.h"
+#include <vector>
+int main () {
+    std::vector<std::vector<int>> ops = {
+            {0},
+            {1},
+            {1,2},
+            {2,3},
+            {4,0},
+            {1,2,4},
+            {2,3,4},
+            {4}
+    };
+    Circuit circ(ops);
+    circ.add_operation({3,2,1});
+    circ.print_operations();
+    circ.print_lines();
 }
